9914eaa1eac2194bab5a1aae694db7f67ce930ce
Peter Karabinovych <petr.karabinovic@live.com>
2016-12-20 09:27:35 +0200

    fix

--- js/app/app.requests.js
+++ js/app/app.requests.js
@@ -6,13 +6,13 @@ app.factory('requests', function($http, actions){
         switch(action.type) {
             case TENANT_SAVE:
                 store.next({type: TENANT_SAVING});
-                $http.post(/tenants/ + action.payload.id, action.payload.geometry)
+                $http.post('tenants/' + action.payload.id, action.payload.geometry)
                      .then(function(){
                          store.next({type: TENANT_HAS_SAVED, payload: action.payload});
                      });
                 return;
             case INIT_ACTION: 
-                $http.get('/tenants/').then(function(d){
+                $http.get('tenants/').then(function(d){
                     _.each(d.data, function(t, id){
                         t.id = id;
                     });
